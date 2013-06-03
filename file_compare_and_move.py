d576e3c4090e55db7e5fb697093315eb1bb387cd
Elliot Lockerman <elliot.lockerman.info>
2013-06-03 18:14:31 -0400

    Added ignore set input

--- file_compare_and_move.py
+++ file_compare_and_move.py
@@ -10,29 +10,36 @@ from shutil import move
 
 
 folder_duplicate = input('Enter the duplicate folder path. Duplicates found in this folder will be moved: ')
-print('folder_duplicate = ' + folder_duplicate)
+print('    folder_duplicate: ' + folder_duplicate, end="\n\n")
 
 folder_original = input('Enter original folder path: ')
-print('folder_original = ' + folder_original)
+print('    folder_original: ' + folder_original, end="\n\n")
 
+ignore_map = set(input('Enter files to be ignored, space delimited. .DS_Store is ignored by default: ').split(sep=' '))
+ignore_map.add('.DS_Store')
+print('    Ignoring: ',end="") 
+print(ignore_map, end="\n\n")
 
 folder_duplicate_files = set(os.listdir(path=folder_duplicate))
 
 folder_original_files = set(os.listdir(path=folder_original))
 
 
-
 folder_separated = os.path.dirname(folder_duplicate) + '/separated'
 if not os.path.exists(folder_separated):
     os.makedirs(folder_separated)
-print('folder_separated = ' + folder_separated)
+print('folder_separated: ',end="")
+print(folder_separated)
 
 
-actual_duplicate_set = folder_duplicate_files & folder_original_files
+actual_duplicate_set = (folder_duplicate_files & folder_original_files) 
+actual_duplicate_set = actual_duplicate_set - ignore_map
+
 
 for actual_duplicate_filename in actual_duplicate_set:
-    if not actual_duplicate_filename == '.DS_Store':
-        print('Duplicate Detected! Moving ' + actual_duplicate_filename)
-        move(folder_duplicate + '/' + actual_duplicate_filename, folder_separated)
+    print('Duplicate Detected! Moving ' + actual_duplicate_filename)
+    move(folder_duplicate + '/' + actual_duplicate_filename, folder_separated)
     
+
+
 print('All done!')
