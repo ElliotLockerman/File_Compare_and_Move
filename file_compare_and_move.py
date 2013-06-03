4230c63297e6f6786844947a68fc4d83a57fa05a
Elliot Lockerman <elliot.lockerman.info>
2013-06-03 19:20:19 -0400

    Completed Script

--- file_compare_and_move.py
+++ file_compare_and_move.py
@@ -5,9 +5,12 @@ NOT recursive, i.e. if two folders have a matching name, the entire folder will 
 Ignores .DS_Store files (Mac's folder attributes file)
 '''
 
+
 import os
 from shutil import move
 
+
+# Get folder_duplicate
 folder_not_entered = True
 while folder_not_entered:
     folder_duplicate = input('Enter the duplicate folder path. Duplicates found in this folder will be moved: ')
@@ -17,6 +20,8 @@ while folder_not_entered:
     else:
         print('Folder does not exist. Please try again')
 
+
+#Get folder_duplicate
 folder_not_entered = True
 while folder_not_entered:
     folder_original = input('Enter original folder path: ')
@@ -26,16 +31,20 @@ while folder_not_entered:
     else:
         print('Folder does not exist. Please try again')
 
+
+#Get ignore_map
 ignore_map = set(input('.DS_Store is ignored by default.\nEnter addition files to be ignored, space delimited\n(If there are no additional files to be ignored, press enter): ').split(sep=' '))
 ignore_map.add('.DS_Store')
 print('    Ignoring: ',end="") 
 print(ignore_map, end="\n\n")
 
-folder_duplicate_files = set(os.listdir(path=folder_duplicate))
 
+#Create sets from lists
+folder_duplicate_files = set(os.listdir(path=folder_duplicate))
 folder_original_files = set(os.listdir(path=folder_original))
 
 
+#Get folder_seperated
 folder_separated = os.path.dirname(folder_duplicate) + '/separated'
 if not os.path.exists(folder_separated):
     os.makedirs(folder_separated)
@@ -43,10 +52,12 @@ print('folder_separated: ',end="")
 print(folder_separated)
 
 
+#Compare folders and create set of duplicates
 actual_duplicate_set = (folder_duplicate_files & folder_original_files) 
 actual_duplicate_set = actual_duplicate_set - ignore_map
 
 
+#Move duplicates
 for actual_duplicate_filename in actual_duplicate_set:
     print('Duplicate Detected! Moving ' + actual_duplicate_filename)
     move(folder_duplicate + '/' + actual_duplicate_filename, folder_separated)
