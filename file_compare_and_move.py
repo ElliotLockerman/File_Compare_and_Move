91d15be4c4936e3835b4b5681822a9e3c6e1d659
Elliot Lockerman <elliot.lockerman.info>
2013-06-03 17:10:46 -0400

    Changed from lists to sets

--- file_compare_and_move.py
+++ file_compare_and_move.py
@@ -1,33 +1,38 @@
+#! /usr/bin/env python3
 '''
-Given two folders as input, folder_duplicate and folder_original, any files or folders in folder_duplicate that have a match in folder_original are moved to a new folder, 'seperated'.
+Given two folders as input, folder_duplicate and folder_original, any files or folders in folder_duplicate that have a match in folder_original are moved to a new folder, 'separated'.
 NOT recursive, i.e. if two folders have a matching name, the entire folder will be moved, regardless of its contents.
-Ignores .DS_Store files (Mac's folder atributes file)
+Ignores .DS_Store files (Mac's folder attributes file)
 '''
 
 import os
 from shutil import move
 
 
-folder_duplicate = input('Enter folder_duplicate path. Duplicates found in this folder will be moved: ')
-folder_duplicate_files = os.listdir(path=folder_duplicate)
+folder_duplicate = input('Enter the duplicate folder path. Duplicates found in this folder will be moved: ')
 print('folder_duplicate = ' + folder_duplicate)
 
-folder_original = input('Enter folder_original path: ')
-folder_original_files = os.listdir(path=folder_original)
+folder_original = input('Enter original folder path: ')
 print('folder_original = ' + folder_original)
 
-folder_seperated = os.path.dirname(folder_duplicate) + '/seperated'
-if not os.path.exists(folder_seperated):
-    os.makedirs(folder_seperated)
-print('folder_seperated = ' + folder_seperated)
 
-for possible_duplicate_file in folder_duplicate_files:
-    print('Checking possible_duplicate_file ' + possible_duplicate_file) 
-    if not possible_duplicate_file == '.DS_Store':
-        for possible_original_file in folder_original_files:
-            if not possible_original_file == '.DS_store':
-                if possible_duplicate_file == possible_original_file:
-                    print('Duplicate Detected! Moving ' + possible_duplicate_file)
-                    move(folder_duplicate + '/' + possible_duplicate_file, folder_seperated)
+folder_duplicate_files = set(os.listdir(path=folder_duplicate))
 
+folder_original_files = set(os.listdir(path=folder_original))
+
+
+
+folder_separated = os.path.dirname(folder_duplicate) + '/separated'
+if not os.path.exists(folder_separated):
+    os.makedirs(folder_separated)
+print('folder_separated = ' + folder_separated)
+
+
+actual_duplicate_set = folder_duplicate_files & folder_original_files
+
+for actual_duplicate_filename in actual_duplicate_set:
+    if not actual_duplicate_filename == '.DS_Store':
+        print('Duplicate Detected! Moving ' + actual_duplicate_filename)
+        move(folder_duplicate + '/' + actual_duplicate_filename, folder_separated)
+    
 print('All done!')
