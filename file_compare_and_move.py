2ca8a0223b91a3505f633aa402f9041518d90294
Elliot Lockerman <elliot.lockerman.info>
2013-06-07 15:42:07 -0400

    All is soft-wrapped, but more formatting and commenting.

--- file_compare_and_move.py
+++ file_compare_and_move.py
@@ -11,7 +11,9 @@ from tkinter import filedialog
 from tkinter import *
 
 
+# Functions
 
+# Functions to call the select directory dialog. One for each button/folder
 def get_folder_duplicate():
     folder_duplicate.set(filedialog.askdirectory())
 
@@ -31,7 +33,6 @@ def compare_and_move(folder_duplicate_function,folder_original_function,
     folder_original_str=folder_original_function.get()
     folder_separated_str=folder_separated_function.get()   
     ignore_map_str = set(ignore_map_function.get().split(sep=','))
-    # NEED TO CHECK IF DIRECTORIES EXIST
 
     # Check if directories exist
     if not os.path.exists(folder_duplicate_str):
@@ -48,8 +49,7 @@ def compare_and_move(folder_duplicate_function,folder_original_function,
         fail_window.resizable(FALSE,FALSE)
         fail_frame = ttk.Frame(fail_window, padding="10 10 10 10")
         fail_frame.grid(column=0, row=0, sticky=(W, N, E, S))
-        ttk.Label(fail_frame, text="The folder with originals you specified" \
-            " does not exist. Please try again").grid(column=0, row=0, pady=10)
+        ttk.Label(fail_frame, text="The folder with originals you specified does not exist. Please try again").grid(column=0, row=0, pady=10)
         ttk.Button(fail_frame, text="Ok",
                    command=lambda: fail_window.destroy()).grid(column=0, row=2)
         return
@@ -58,8 +58,7 @@ def compare_and_move(folder_duplicate_function,folder_original_function,
         fail_window.resizable(FALSE,FALSE)
         fail_frame = ttk.Frame(fail_window, padding="10 10 10 10")
         fail_frame.grid(column=0, row=0, sticky=(W, N, E, S))
-        ttk.Label(fail_frame, text="The folder to move to you specified" \
-            " does not exist. Please try again").grid(column=0, row=0, pady=10)
+        ttk.Label(fail_frame, text="The folder to move to you specified does not exist. Please try again").grid(column=0, row=0, pady=10)
         ttk.Button(fail_frame, text="Ok",
                    command=lambda: fail_window.destroy()).grid(column=0, row=2)
         return
@@ -69,10 +68,8 @@ def compare_and_move(folder_duplicate_function,folder_original_function,
     prog_window.resizable(FALSE,FALSE)
     prog_frame = ttk.Frame(prog_window, padding="10 10 10 10")
     prog_frame.grid(column=0, row=0, sticky=(W, N, E, S))
-    ttk.Label(prog_frame, text="Searching for and moving duplicates ").grid(
-        column=1, row=0, padx="15", pady="15", sticky=E)
-    prog_bar = ttk.Progressbar(prog_frame, orient="horizontal", 
-                               length=250, mode='indeterminate')
+    ttk.Label(prog_frame, text="Searching for and moving duplicates ").grid(column=1, row=0, padx="15", pady="15", sticky=E)
+    prog_bar = ttk.Progressbar(prog_frame, orient="horizontal", length=250, mode='indeterminate')
     prog_bar.grid(column=0, row=1, columnspan=3)
     prog_bar.start()
     
@@ -99,11 +96,13 @@ def compare_and_move(folder_duplicate_function,folder_original_function,
     done_frame = ttk.Frame(done_window, padding="10 10 10 10")
     done_frame.grid(column=0, row=0, sticky=(W, N, E, S))
     ttk.Label(done_frame, text="All Done!").grid(column=0, row=0, pady=10)
-    ttk.Button(done_frame, text="Ok", 
-               command=lambda: done_window.destroy()).grid(column=0, row=2)
+    ttk.Button(done_frame, text="Ok", command=lambda: done_window.destroy()).grid(column=0, row=2)
+# End function compare_and_move
+    
 
-    
-    
+
+
+# UI    
     
 root = Tk()
 root.title("File Compare and Move")
@@ -132,53 +131,42 @@ mainframe.rowconfigure(5, weight=0)
 
 
 
-# Elements
+# UI Elements
+
 # Folder Duplicate
 ttk.Label(mainframe, text="Folder with duplicates: ") \
     .grid(column=0, row=1, padx="15", pady="15", sticky=E)
 folder_duplicate = StringVar()
-ttk.Entry(mainframe, textvariable=folder_duplicate) \
-    .grid(column=1, row=1, columnspan=2, padx="15", pady="15", sticky=W+E)
+ttk.Entry(mainframe, textvariable=folder_duplicate).grid(column=1, row=1, columnspan=2, padx="15", pady="15", sticky=W+E)
 ttk.Button(mainframe, text="Select Folder...", 
-           command=lambda: get_folder_duplicate()) \
-    .grid(column=3, row=1, sticky=W, padx="15", pady="15")
+           command=lambda: get_folder_duplicate()).grid(column=3, row=1, sticky=W, padx="15", pady="15")
 
 
 # Folder Original
-ttk.Label(mainframe, text="Folder with originals: ") \
-    .grid(column=0, row=2, padx="15", pady="15", sticky=E)
+ttk.Label(mainframe, text="Folder with originals: ").grid(column=0, row=2, padx="15", pady="15", sticky=E)
 folder_original = StringVar()
-ttk.Entry(mainframe, textvariable=folder_original) \
-    .grid(column=1, row=2, columnspan=2, padx="15", pady="15", sticky=W+E)
+ttk.Entry(mainframe, textvariable=folder_original).grid(column=1, row=2, columnspan=2, padx="15", pady="15", sticky=W+E)
 ttk.Button(mainframe, text="Select Folder...", 
-           command=lambda: get_folder_original()) \
-    .grid(column=3, row=2, padx="15", pady="15")
+           command=lambda: get_folder_original().grid(column=3, row=2, padx="15", pady="15")
 
 # Folder Separated
-ttk.Label(mainframe, text="Folder to move duplicates to: ") \
-    .grid(column=0, row=3, padx="15", pady="15", sticky=E)
+ttk.Label(mainframe, text="Folder to move duplicates to: ").grid(column=0, row=3, padx="15", pady="15", sticky=E)
 folder_separated = StringVar()
-ttk.Entry(mainframe, textvariable=folder_separated) \
-    .grid(column=1, row=3, columnspan=2, padx="15", pady="15", sticky=W+E)
+ttk.Entry(mainframe, textvariable=folder_separated).grid(column=1, row=3, columnspan=2, padx="15", pady="15", sticky=W+E)
 ttk.Button(mainframe, text="Select Folder...", 
-           command= lambda: get_folder_separated()) \
-    .grid(column=3, row=3, padx="20", pady="20")
+           command= lambda: get_folder_separated()).grid(column=3, row=3, padx="20", pady="20")
 
 
 # Ignore map
-ttk.Label(mainframe, text="Files to ignore(comma delimited, no spaces): ") \
-    .grid(column=0, row=4, padx="15", pady="15", sticky=E)
+ttk.Label(mainframe, text="Files to ignore(comma delimited, no spaces): ").grid(column=0, row=4, padx="15", pady="15", sticky=E)
 ignore_map = StringVar()
 ignore_map.set(".DS_Store,")
-ttk.Entry(mainframe, textvariable=ignore_map) \
-    .grid(column=1, row=4, columnspan=2, sticky=W+E)
+ttk.Entry(mainframe, textvariable=ignore_map).grid(column=1, row=4, columnspan=2, sticky=W+E)
 
 
 # Execute Button
 ttk.Button(mainframe, text="Find and Move!", 
-           command= lambda: compare_and_move(folder_duplicate,folder_original,
-                                             folder_separated,ignore_map)) \
-    .grid(column=3, row=5, sticky=(W, E), padx="20", pady="30")
+           command= lambda: compare_and_move(folder_duplicate,folder_original,folder_separated,ignore_map)).grid(column=3, row=5, sticky=(W, E), padx="20", pady="30")
 
 
 root.mainloop()
